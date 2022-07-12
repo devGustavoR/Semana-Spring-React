12f75b1c39463ba48838ebf4fbdffd3a907a69dd
devgustavor <devgustavor@gmail.com>
2022-07-12 11:05:26 -0300

    Project created

---
+++ backend/src/test/java/com/devgustavor/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgustavor.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
